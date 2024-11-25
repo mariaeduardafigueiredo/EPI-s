f6b073d1d9bd416977421651b43c4107e9fcb0a9
Maria Eduarda Figueiredo <128522652+mariaeduardafigueiredo@users.noreply.github.com>
2024-11-25 20:48:04 -0300

    Delete Imersão5 directory

--- Imersão5/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from app.models import Usuario, Equipamento, Emprestimo
-
-admin.site.register(Usuario)
-admin.site.register(Equipamento)
-admin.site.register(Emprestimo)
